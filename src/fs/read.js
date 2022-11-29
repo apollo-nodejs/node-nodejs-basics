1b28791a438ea80c98ebcb0d75f3fa06bb0ff2b1
Atlas_WebDev <web.dev89@yandex.by>
2022-11-30 01:25:06 +0300

    node:refactor

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import fs from 'node:fs/promises';
 const read = async () => {
-    // Write your code here 
+    const pathFile = 'src/fs/files/fileToRead.txt';
+    const failed = "FS operation failed";
+    // check file
+    const isFile = async (path, failedMessage) => {
+        try {
+            await fs.access(path, fs.constants.F_OK);
+        }catch (error) {
+            throw new Error (failedMessage);
+        }
+    }
+
+    try {
+        await isFile(pathFile, failed);
+        const data = await fs.readFile(pathFile, { encoding: 'utf8' });
+        console.log(data);
+    }catch (e) {
+        console.log(e.message);
+    }
 };
 
 await read();
