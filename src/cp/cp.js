852d48c0aedc6b7f6c6762e0c6a6d1f2ad44ff20
Atlas_WebDev <web.dev89@yandex.by>
2022-12-05 21:05:00 +0300

    finish

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,11 @@
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child_process = await import('node:child_process');
+    const child = './src/cp/files/script.js';
+    const arg = process.argv.slice(2);
+
+    // create child process
+    const child_1 = child_process.fork(child, arg);
+    child_1.send(process.stdin);
 };
 
 spawnChildProcess();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,17 @@
+import { workerData, parentPort } from 'worker_threads';
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
-
 const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+        if (typeof workerData === 'number') {
+            const result = nthFibonacci(workerData);
+            if(result) {
+                parentPort.postMessage({ status: 'resolved', data: result });
+            }else {
+                parentPort.postMessage({ status: 'error', data: 'null' });
+            }
+        }else {
+            parentPort.postMessage({ status: 'error', data: 'not number' });
+        }
 };
-
 sendResult();
